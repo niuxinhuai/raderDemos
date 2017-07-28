f39e59c7b627e424bdf354432ab0eec13b31d256
niuxinhuaie <1242537720@qq.com>
2017-07-28 11:27:46 +0800

    雷达扫描图实现

---
+++ RadarDemo/RadarDemoTests/RadarDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  RadarDemoTests.m
+//  RadarDemoTests
+//
+//  Created by apple on 16/1/13.
+//  Copyright © 2016年 吴凯锋 QQ:24272779. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RadarDemoTests : XCTestCase
+
+@end
+
+@implementation RadarDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
